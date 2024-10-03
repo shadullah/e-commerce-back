83eea632d825d9f463f8ff8549aba34a236de9e6
hexaalif <shadullahsad9@gmail.com>
2024-10-03 17:09:19 +0600

    vercel push 2

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
