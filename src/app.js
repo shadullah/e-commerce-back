73a6d8077023a3ab9dff1fe4483125d24f89a05f
hexaalif <shadullahsad9@gmail.com>
2024-10-03 16:45:07 +0600

    db

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
 );
