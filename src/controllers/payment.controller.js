c70aa54d4a76806fa7799239d4ceca70eca3214d
hexaalif <shadullahsad9@gmail.com>
2024-11-22 20:15:59 +0600

    order and payment controller working with wach other

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -2,48 +2,52 @@ import mongoose from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import axios from "axios";
 import { Payment } from "../models/payment.model.js";
+import { Order } from "../models/order.model.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const paymentInitiate = asyncHandler(async (req, res) => {
-  const { customer, orderPrice } = req.body;
+  const { orderId } = req.body;
+
+  const order = await Order.findById(orderId);
+  if (!order) {
+    throw new ApiError(400, "Order not found");
+  }
 
   const trxId = new mongoose.Types.ObjectId().toString();
 
   const initiateData = {
     store_id: "ss66f78af2baee0",
     store_passwd: "ss66f78af2baee0@ssl",
-    total_amount: orderPrice,
+    total_amount: order.orderPrice,
     // orderPrice: payInfo.orderPrice,
     currency: "BDT",
     tran_id: trxId,
     success_url: "http://localhost:8000/success",
     fail_url: "http://localhost:8000/failed",
     cancel_url: "http://localhost:8000/cancelled",
-    cus_name: "Customer Name",
+    cus_name: order.customer,
     cus_email: "cust@yahoo.com",
-    cus_add1: "Dhaka",
-    cus_add2: "Dhaka",
-    cus_city: "Dhaka",
-    cus_state: "Dhaka",
-    cus_postcode: "1000",
-    cus_country: "Bangladesh",
-    cus_phone: "01711111111",
-    cus_fax: "01711111111",
-    ship_name: "Customer Name",
-    ship_add1: "Dhaka",
-    ship_add2: "Dhaka",
+    cus_add1: order.address,
+    cus_city: order.city,
+    cus_postcode: order.zip,
+    cus_phone: order.phone,
+    // cus_fax: "01711111111",
+    // ship_name: "Customer Name",
+    // ship_add1: "Dhaka",
+    // ship_add2: "Dhaka",
     shipping_method: "No",
     product_name: "My Product",
     product_category: "electro",
     product_profile: "general",
-    ship_city: "Dhaka",
-    ship_state: "Dhaka",
-    ship_postcode: "1000",
-    ship_country: "Bangladesh",
-    multi_card_name: "mastercard,visacard,amexcard",
-    value_a: "ref001_A",
-    value_b: "ref002_B",
-    value_c: "ref003_C",
-    value_d: "ref004_D",
+    // ship_city: "Dhaka",
+    // ship_state: "Dhaka",
+    // ship_postcode: "1000",
+    // ship_country: "Bangladesh",
+    // multi_card_name: "mastercard,visacard,amexcard",
+    // value_a: "ref001_A",
+    // value_b: "ref002_B",
+    // value_c: "ref003_C",
+    // value_d: "ref004_D",
   };
 
   try {
@@ -59,9 +63,10 @@ const paymentInitiate = asyncHandler(async (req, res) => {
     // console.log(amount);
 
     const newPayment = new Payment({
-      customer,
+      customer: order.customer,
       paymentId: trxId,
-      orderPrice,
+      orderPrice: order.orderPrice,
+      orderId: order._id,
     });
 
     const saveRes = await newPayment.save();
